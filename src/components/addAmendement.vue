4c9bb5bba811ed355699217e5c6721bf6027d662
Angelo Matte <amatteLISST>
2026-09-25 17:26:37 +0200

    better form

--- src/components/addAmendement.vue
+++ src/components/addAmendement.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useAuth } from '@/composables/useAuth.js';
 import { sendToSupabase } from './../supabase.js';
 
@@ -25,8 +25,6 @@ if(props.scenario.SCENARIO_CODE !== 0) {
     ]
 };
 
-const curr_NAT_AMEND = ref(choix_NAT_AMEND[0])
-
 const detailAmendement = ref({
     TITRE_AMEND: "",
     NAT_AMEND: "",
@@ -37,6 +35,19 @@ const detailAmendement = ref({
     ADMIN: "",
     PORT: "",
     FINALISE: true,
+});
+
+const champsManquants = computed(() => {
+    if (
+        !detailAmendement.value.TITRE_AMEND ||
+        !detailAmendement.value.NAT_AMEND ||
+        !detailAmendement.value.JUSTIFICATION ||
+        !detailAmendement.value.PORT
+    ) {
+        return true;
+    }
+
+    return false;
 });
 
 const sendMessage = async () => {
@@ -73,15 +84,16 @@ const sendMessage = async () => {
                         <label for="TITRE_AMEND">Titre de l'amendement*</label>
                         <input type="text" name="" id="TITRE_AMEND" v-model="detailAmendement.TITRE_AMEND" placeholder="En une phrase courte, écrivez l'idée de l'amendement" />
 
-                        <label for="NATURE_AMEND">Nature de l'amendement</label>
-                        <select :disabled="scenario.SCENARIO_CODE===0 ? true:false" id="NAT_AMEND" v-model="curr_NAT_AMEND">
+                        <label for="NATURE_AMEND">Nature de l'amendement*</label>
+                        <select :disabled="scenario.SCENARIO_CODE===0 ? true:false" id="NAT_AMEND" v-model="detailAmendement.NAT_AMEND">
+                            <option value="">Spécifiez la nature de l'amendement.</option>
                             <option v-for="choix in choix_NAT_AMEND" :value="choix">{{ choix }}</option>
                         </select>
 
                         <label for="TEXT_AMEND">Décrivez le contenu de l'amendement</label>
                         <textarea name="" id="TEXTE_AMEND" v-model="detailAmendement.TEXTE_AMEND" placeholder="Écrivez la proposition telle que vous voudriez la voir ajoutée au scénario. Précisez les passages actuels du scénario que vous voudriez enlever ou remplacer."></textarea>
 
-                        <label for="JUSTIFICATION">Justification de l'amendement</label>
+                        <label for="JUSTIFICATION">Justification de l'amendement*</label>
                         <textarea name="" id="JUSTIFICATION" v-model="detailAmendement.JUSTIFICATION" placeholder="Expliquez pourquoi cet amendement vous semble souhaitable. S'il s'inscrit dans un scénario existant, expliquez en quoi il correspond à la logique d'ensemble du scénario (i.e. pourquoi dans ce scénario et pas un autre)."></textarea>
 
                         <label for="RISQUE">Risques anticipés</label>
@@ -90,24 +102,28 @@ const sendMessage = async () => {
                         <label for="ADMIN">Implémentation administrative</label>
                         <textarea id="ADMIN" v-model="detailAmendement.ADMIN" placeholder="Précisez quel entité (direction, conseil de labo, gestionnaire, etc.) prend en charge l'application, et si besoin avec quels outils. Soyez aussi spécifique que possible."></textarea>
                         
-                        <p>Finalisé</p>
-                        <div>
-                            <label for="TRUE" value="true">Oui</label>
-                            <input type="radio" name="FINALISE" @click="detailAmendement.FINALISE=true" checked>
+                        <div id="radioContainer">
+                            <p>Finalisé</p>
+                            <div>
+                                <label for="TRUE" value="true">Oui</label>
+                                <input type="radio" name="FINALISE" @click="detailAmendement.FINALISE=true" checked>
+                            </div>
+                            <div>
+                                <label for="FALSE" value="false" @click="detailAmendement.FINALISE=false">Non</label>
+                                <input type="radio" name="FINALISE">
+                            </div>
                         </div>
-                        <div>
-                            <label for="FALSE" value="false" @click="detailAmendement.FINALISE=false">Non</label>
-                            <input type="radio" name="FINALISE">
+                        <div id="endForm">
+                            <div>
+                                <label for="PORT">Porteur·euse de l'amendement*</label>
+                                <input type="text" name="" id="PORT" v-model="detailAmendement.PORT" placeholder="Saisissez votre nom et votre email." />
+                            </div>
+                            <button :disabled="champsManquants">
+                                <span v-if="champsManquants" class="tooltip" aria-label="Il manque des champs obligatoires">Soumettre la proposition</span>
+                                <span v-else>Soumettre la proposition</span>
+                            </button>
                         </div>
-
-                        <label for="PORT">Porteur·euse de l'amendement*</label>
-                        <input type="text" name="" id="PORT" v-model="detailAmendement.PORT" placeholder="Saisissez votre nom et votre email." />
-
                         <p><small>* Champs obligatoires</small></p>
-                        <button :disabled="detailAmendement.TITRE_AMEND ? false:true">
-                            <span v-if="!detailAmendement.TITRE_AMEND" class="tooltip" aria-label="Il manque des champs obligatoires">Soumettre la proposition</span>
-                            <span v-else>Soumettre la proposition</span>
-                        </button>
                     </fieldset>
 
                 </form>
@@ -143,9 +159,32 @@ const sendMessage = async () => {
         margin-top: 10px;
     }
 
+    form {
+        width: 95%;
+        height: 95%;
+        margin: auto;
+    }
+
     form button {
         background-color: #2f9e44;
         color: #fff
+    }
+
+    #radioContainer {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    #radioContainer div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #endForm{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
     }
 
 </style>
